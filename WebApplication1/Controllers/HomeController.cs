9e4334de53e1beca01af0ebf90cd3913b219dce6
Roksolana Kril <roksolana.kr@gmail.com>
2017-07-19 23:47:55 +0300

    init2

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ViewResult Index()
+        {
+            int Hour = DateTime.Now.Hour;
+            ViewBag.Greeting = Hour < 12 ? "GOOD MORNING" : "GOOD EVENING";
+            return View();
+        }
+
+        public ViewResult RSVPForm()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/Models/GuestResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class GuestResponse
+    {
+        public string Name;
+        public string AdditionalInfo;
+        public int Phone;
+        public bool? WillAttend;
+        public string Email;
+        public DateTime Date;
+    }
+}
